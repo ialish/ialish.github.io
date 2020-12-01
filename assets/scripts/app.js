ba0036afaf61f2182d82e797362d0dcbd499d57f
Eyal <eyal.shaar@gmail.com>
2020-12-02 00:53:34 +0200

    Update

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,6 @@
 Typed.new("#writing-text", {
 	strings: [
-		"I have a B.Sc. degree in Electrical Engineering", "I'm a Software Developer trained in C/C++", "I'm a Full-Stack Web Developer"
+		"I'm a Full-Stack Developer"
 	], // strings to be typed
 	stringsElement: null, // ID of element containing string children
 	typeSpeed: 20, // type speed in milliseconds
